252b58e8fde7ada58def76e674ed9ba5a918650c
shimonyg <shimonyg@post.bgu.ac.il>
2020-11-20 19:19:35 +0200

    replace RT and term_dict for quoted URLs

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import parser_module
 
 if __name__ == '__main__':
 
-    #search_engine.main()
+    search_engine.main()
     parser = parser_module.Parse()
     parser.parse_sentence("#HelloBitch #heyThere 1000 Million"
                           ,False)
