b42b96f7f2e785195f12bc14577f48b8b2931a63
edenyav <edenyav@post.bgu.ac.il>
2021-01-07 21:36:42 +0200

    updated the google drive in the configuration.py

--- Part C/configuration.py
+++ Part C/configuration.py
@@ -4,7 +4,7 @@ import os
 class ConfigClass:
     def __init__(self):
         # link to a zip file in google drive with your pretrained model
-        self._model_url = "https://drive.google.com/drive/folders/1XXCZLufsPvZmTPYmRSF32qi5gmKlj3Jx?usp=sharing"
+        self._model_url = "https://drive.google.com/file/d/1kzcqmnWssQDERST1BN4_5mVTd-uW3rxG/view?usp=sharing"
         # False/True flag indicating whether the testing system will download 
         # and overwrite the existing model files. In other words, keep this as 
         # False until you update the model, submit with True to download 
